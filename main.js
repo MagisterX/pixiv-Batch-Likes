0107ebbd1da3f2d3847044b3205407e5d7a33c16
AndyTLemon <90253895+AndyTLemon@users.noreply.github.com>
2023-03-12 11:54:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            pixiv批量點讚
 // @namespace       https://github.com/AndyTLemon/pixiv-Batch-Likes.git
-// @version         1.2
+// @version         1.3
 // @description     批量作品點讚
 // @author          AndyTLemon
 // @match           *www.pixiv.net/*/*/*
@@ -10,14 +10,45 @@
 // ==/UserScript==
 
 const wait = ms => new Promise(res => setTimeout(res, ms));
+let isrunning = false;
+
+//button
+const btn = `<nav name = "ClickLikeBtnNav" class="sc-192ftwf-0 kWAFb" style="justify-content: center;">
+<button class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
+border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(false)">點讚一頁</button>
+<button class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
+border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(true)">點讚全部</button>
+</nav>`;
+
+
+/* function stopbtnfunction() {
+    if (!isrunning) {
+        return
+    };
+//end btnfunction;
+};
+window.stopbtnfunction = stopbtnfunction; */
 
 async function btnfunction(isall) {
+    if (isrunning) {
+        return;
+    };
+    isrunning = true;
+    /*     //stopbtn
+        const stopbtn = `<button name = "stopbtn" class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
+        border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="stopbtnfunction()">停止點讚</button>`;
+
+        const stopbtnLocation = document.getElementsByName("ClickLikeBtnNav");
+        for (let item of stopbtnLocation) {
+            item.insertAdjacentHTML("beforeend", stopbtn);
+        }; */
+
 
 
     let arrow = document.getElementsByClassName("kKBslM");
 
     //back to page 1
-    if (isShow(arrow[0])) {
+    if (isShow(arrow[0]) && isall) {
         document.getElementsByTagName("nav")[2].getElementsByTagName("a")[1].click();
         console.log("back to page 1");
         await wait(Math.random() * 1000 + 500 | 0);
@@ -25,7 +56,7 @@ async function btnfunction(isall) {
 
     //click like
     while (true) {
-        let s = document.getElementsByClassName("dxYRhf");
+        const s = document.getElementsByClassName("dxYRhf");
 
         for (let index = 0; index < s.length; index++) {
             s[0].closest("button").click();
@@ -41,6 +72,11 @@ async function btnfunction(isall) {
         };
     };
 
+    /*     //del stopbtn
+        for (let item of stopbtnLocation) {
+            item.lastChild.remove();
+        }; */
+    isrunning = false;
     console.log("done")
 };
 window.btnfunction = btnfunction;
@@ -50,20 +86,23 @@ function isShow(el) {
     return (style.display === 'flex')
 };
 
+async function placebtn() {
+    let isplace = false
+    try {
+        await wait(1000);
+        let btnLocaton = document.getElementsByClassName("sc-192ftwf-0 kWAFb")[0];
+        let btnLocaton2 = document.getElementsByClassName("sc-xhhh7v-0 kYtoqc")[0];
+        console.log("trying");
+        btnLocaton.insertAdjacentHTML("beforeend", btn);
+        btnLocaton2.insertAdjacentHTML("afterend", btn);
+        isplace = true
+    } catch (error) {
+        if (error && !isplace) {
+            placebtn();
+        }
+    }
+}
+
 (async function () {
-
-    //button
-    const btn = `<nav class="sc-192ftwf-0 kWAFb" style="justify-content: center;">
-<button class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
-border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(false)">點讚一頁</button>
-<button class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
-border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(true)">點讚全部</button>
-</nav>`;
-    await wait(5000);
-    let btnLocaton = document.getElementsByClassName("sc-192ftwf-0 kWAFb")[0]
-    btnLocaton.insertAdjacentHTML("beforeend", btn);
-    btnLocaton = document.getElementsByClassName("sc-xhhh7v-0 kYtoqc")[0]
-    btnLocaton.insertAdjacentHTML("afterend", btn);
-
-    console.log("done");
+    placebtn()
 })();
