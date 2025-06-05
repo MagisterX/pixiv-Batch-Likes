80084f328b31c3f5200d7af377c9a922647ad45b
MagisterX <36625965+MagisterX@users.noreply.github.com>
2025-06-05 07:28:07 +0400

    1.5.3

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name            pixiv批量點讚
 // @namespace       https://github.com/AndyTLemon/pixiv-Batch-Likes.git
-// @version         1.4.2
+// @version         1.5.3
 // @description     批量作品點讚
-// @author          AndyTLemon
+// @author          AndyTLemon 1.4.2; MagisterX 1.5+
 // @match           *www.pixiv.net/*/*/*
+// @include         *www.pixiv.net/en/*
 // @grant           none
 // @icon            https://www.pixiv.net/favicon.ico
 // ==/UserScript==
@@ -12,13 +13,14 @@
 const wait = ms => new Promise(res => setTimeout(res, ms));
 let isrunning = false;
 let stopnow = false;
+let isplace = false;
 
 //button
 const btn = `<nav name = "ClickLikeBtnNav" class="sc-192ftwf-0 kWAFb" style="justify-content: center;">
 <button class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
-border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(false)">點讚一頁</button>
+border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(false)">Like a page</button>
 <button class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
-border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(true)">點讚全部</button>
+border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="btnfunction(true)">Like all</button>
 </nav>`;
 
 
@@ -40,44 +42,54 @@ async function btnfunction(isall) {
 
     //stopbtn
     const stopbtn = `<button name = "stopbtn" class="sc-d98f2c-1 sc-192ftwf-1 ioZtRi" style="background-color: transparent;background-repeat: no-repeat; border: 2px solid #3acfff;
-        border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="stopbtnfunction()">停止點讚</button>`;
+        border-radius: 100px;cursor: pointer;overflow: hidden;outline: none;" onclick="stopbtnfunction()">Stop</button>`;
+
+    let allImages = document.getElementsByClassName("gQCZLY");
+    if (allImages.length > 0) {
+        console.log("Going to page 1");
+        allImages[0].click();
+        await wait(3000 | 0);
+       };
 
     const stopbtnLocation = document.getElementsByName("ClickLikeBtnNav");
     for (let item of stopbtnLocation) {
         item.insertAdjacentHTML("beforeend", stopbtn);
     };
 
-
-
-    let arrow = document.getElementsByClassName("kKBslM");
+    let arrow = document.getElementsByClassName("dDrHMO");
 
     //back to page 1
-    if (isShow(arrow[0]) && isall) {
+    if (arrow.length > 0 && isShow(arrow[0]) && isall) {
         document.getElementsByTagName("nav")[2].getElementsByTagName("a")[1].click();
         console.log("back to page 1");
-        await wait(Math.random() * 1000 + 500 | 0);
+        await wait(3000 | 0);
     };
-    const s = document.getElementsByClassName("dxYRhf");
+    const liveElements = document.getElementsByClassName("eRAmZC");
+    const liveElementsHearts = document.getElementsByClassName("cHNSiM");
     //click like
     while (true) {
-
+        if (stopnow) {
+            //del stopbtn
+            for (let item of stopbtnLocation) {
+                item.lastChild.remove();
+            };
+            isrunning = false;
+            return;
+        };
+        if(liveElements.length == 0){
+            await wait(3000 | 0);
+            continue;
+        }
+        const s = Array.from(liveElementsHearts); //staticarray
         let slength = s.length
         for (let i = 0; i < slength; i++) {
-            s[0].closest("button").click();
-            await wait((Math.random() + 2) * 1000 | 0);
-            if (stopnow) {
-                //del stopbtn
-                for (let item of stopbtnLocation) {
-                    item.lastChild.remove();
-                };
-                isrunning = false;
-                return;
-            };
+            s[i].closest("button").click();
+            await wait(1200 | 0);
         }
 
         if (isShow(arrow[1]) && isall) {
             arrow[1].click();
-            await wait((Math.random() + 2) * 1000 | 0);
+            await wait(3000 | 0);
         }
         else {
             break;
@@ -97,20 +109,29 @@ function isShow(el) {
     var style = window.getComputedStyle(el);
     return (style.display === 'flex')
 };
-
-(async function placebtn() {
-    let isplace = false;
-    let btnLocaton = document.getElementsByClassName("sc-192ftwf-0 kWAFb")[0];
-    let btnLocaton2 = document.getElementsByClassName("sc-xhhh7v-0 kYtoqc")[0];
+function placebtn() {
+    let btnLocaton = document.getElementsByClassName("sc-a6755c3a-0 dlidhK")[0];
+    let btnLocaton2 = document.getElementsByClassName("dDrHMO")[1];
     try {
-        await wait(1000);
         //console.log("trying");
+        if (!isplace){
         btnLocaton.insertAdjacentHTML("beforeend", btn);
+        isplace = true;
         btnLocaton2.insertAdjacentHTML("afterend", btn);
-        isplace = true
+        };
     } finally {
-        if (!isplace) {
-            placebtn();
-        }
+        let btns = document.getElementsByClassName("kWAFb");
+        if (btns.length == 0){
+            isplace=false;
+        };
+        return;
+    }
+};
+
+(async function run() {
+    while(true){
+        await wait(1000);
+        placebtn();
+        console.log("trying");
     }
 })();
